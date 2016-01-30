9c113097fb3e03f2ccf5f67b9f73734ffe54154f
Jianbo Ye <jxy198@ist.psu.edu>
2016-01-30 16:49:11 -0500

    add comments

--- OptimalTransport_IBP_Sinkhorn.m
+++ OptimalTransport_IBP_Sinkhorn.m
@@ -6,10 +6,18 @@ m = size(W2,1);
 v=ones(m,1);
 
 xi=exp(-C / rho);
+%umax=zeros(max_iter);
+%vmax=zeros(max_iter);
 for i=1:max_iter
     u=W1 ./ (xi*v);
     v=W2 ./ (xi'*u);
+%    umax(i)=max(u);
+%    vmax(i)=max(v);
 end
 
+%figure; plot(log10(umax), log(vmax), 'd');
+if any(isnan(u)) || any(isnan(v))
+    error('NaN encountered.')
+end
 Pi=diag(u) * xi * diag(v);
 V=sum(C(:).*Pi(:));
